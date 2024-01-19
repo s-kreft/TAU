061f9eb8b714b01d2bc1706db4cd657157276fcb
s-kreft <sebastian.kreft.sg@gmail.com>
2024-01-19 17:39:24 +0100

    Add movement methods

--- Laboratories3/Game.py
+++ Laboratories3/Game.py
@@ -1,16 +1,41 @@
 import numpy as np
 import random
+import keyboard
 
 
 rows = 0
 columns = 0
 board = []
+playerPositionX = 0
+playerPositionY = 0
+endPositionX = 5
+endPositionY = 0
+
+def Game():
+    PrepareGame()
+    Gameplay()
 
 def PrepareGame():
     GenerateBoard()
+    ObstaclesGenerator()
     StartField()
     EndField()
     ShowBoard()
+   # MoveRight()
+
+def Gameplay():
+    while not (playerPositionX == endPositionX and playerPositionY == endPositionY):
+            GameMovements()
+    else:            
+        print("You win!")
+
+    # while not (playerPositionX == endPositionX and playerPositionY == endPositionY):
+    #     if keyboard.is_pressed('q'):
+    #         break
+    #     else:  
+    #         GameMovements()
+    # else:            
+    #     print("You win!")
 
     
 def GenerateBoard():
@@ -23,17 +48,76 @@ def GenerateBoard():
     board[:] = '='
 
 def StartField():
+    global playerPositionY
     randomField = random.randint(0,4)
     board[randomField][0] = 'S'
+    playerPositionY = randomField
 
 def EndField():
+    global endPositionY
     randomField = random.randint(0,4)
     board[randomField][4] = 'E'
+    endPositionY = randomField
+
+def ObstaclesGenerator():
+    obstaclesNumber = 10
+    x = 0
+    while x <= obstaclesNumber:
+        randomRow = random.randint(0,4)
+        randomColumn = random.randint(0,4)
+        board[randomRow][randomColumn] = '@'
+        x +=1
 
 def ShowBoard():
     print(board)
 
-PrepareGame()
+
+def GameMovements():
+    global playerPositionX
+    global playerPositionY
+    global endPositionX
+    global endPositionY
+    global board
+
+    while True:       
+        try:
+            if keyboard.is_pressed('right'):
+                board[playerPositionY][playerPositionX] = '#'
+                playerPositionX += 1
+                board[playerPositionY][playerPositionX] = 'P'
+                print(board)
+                print("prawa szczauka")
+                break
+
+            if keyboard.is_pressed('left'):
+                board[playerPositionY][playerPositionX] = '#'
+                playerPositionX -= 1
+                board[playerPositionY][playerPositionX] = 'P'
+                print(board)
+                print("lewa szczauka")
+                break
+
+            if keyboard.is_pressed('up'):
+                board[playerPositionY][playerPositionX] = '#'
+                playerPositionY -= 1
+                board[playerPositionY][playerPositionX] = 'P'
+                print(board)
+                print("gorna szczauka")
+                break
+
+            if keyboard.is_pressed('down'):
+                board[playerPositionY][playerPositionX] = '#'
+                playerPositionY += 1
+                board[playerPositionY][playerPositionX] = 'P'
+                print(board)
+                print("dolna szczauka")
+                break
+        except:
+            break
+
+
+Game()
+#print(board)
 
 
 
